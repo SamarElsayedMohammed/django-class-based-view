25bf0dd5a1b33d1dcaa3db4e318d1cbbb6121013
SamarElsayedMohammed <samarelsayedmohammed12345@gmail.com>
2021-08-22 05:15:57 +0200

    add class base view

--- post/views.py
+++ post/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView ,DetailView ,CreateView ,UpdateView ,DeleteView 
+from . models import Post
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+
+class PostDetail(DetailView):
+    model = Post
+
+class PostCreate():
+    pass
+
+class PostUpdate():
+    pass
+
+class PostDelete():
+    pass
+
+
+
